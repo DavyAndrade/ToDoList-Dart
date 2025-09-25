ea45fbfc6b8174bb0ce55e3cb18a997088a425fe
DavyAndrade <davy.dama55@gmail.com>
2025-09-25 20:15:13 -0300

    first commit

---
+++ main.dart
@@ -0,0 +1,62 @@
+class TaskManager {
+  List<String> tasks = [];
+
+  void addTask(String task) {
+    tasks.add(task);
+    print('Task added: $task');
+  }
+
+  void readAllTasks() {
+    print('All Tasks:');
+
+    for (var task in tasks) {
+      print('ID: ${tasks.indexOf(task)} — $task');
+    }
+  }
+
+  void findTask(int id) {
+    if (id >= 0 && id < tasks.length) {
+      print('Task found: ${tasks[id]}');
+    } else {
+      print('Task not found at index: $id');
+    }
+  }
+
+  void updateTask(int id, String newTask) {
+    if (id >= 0 && id < tasks.length) {
+      String oldTask = tasks[id];
+      tasks[id] = newTask;
+      print('Task updated: $oldTask to $newTask');
+    } else {
+      print('Task not found at index: $id');
+    }
+  }
+
+  void deleteTask(int id) {
+    if (id >= 0 && id < tasks.length) {
+      String task = tasks[id];
+      tasks.removeAt(id);
+      print('Task deleted: $task');
+    } else {
+      print('Task not found at index: $id');
+    }
+  }
+}
+
+void main() {
+  TaskManager tm = TaskManager();
+
+  print("--- Welcome to Task Manager ---");
+
+  do {
+    print("\nChoose an option:");
+    print("1. Add Task");
+    print("2. Read All Tasks");
+    print("3. Find Task by ID");
+    print("4. Update Task by ID");
+    print("5. Delete Task by ID");
+    print("6. Exit");
+
+    
+  } while (true);
+}
